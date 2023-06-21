7d427beb73832c4b5b157d5ec23064734096191a
= <hardlane2000@gmail.com>
2023-06-22 01:24:28 +0200

    adding database binds config

--- config.py
+++ config.py
@@ -6,21 +6,20 @@ db = SQLAlchemy()
 #configuration for app
 def set_config(app_dict, env):
 
+    BASE_DB = 'sqlite:///database/'
+    tables = ['users', 'blogs', 'items']
     #setting app.config
     config_dict = {
         'SECRET_KEY': 'secretkey1',
         'SQLALCHEMY_DATABASE_URI': 'sqlite:///database/users.db',
         'SQLALCHEMY_TRACK_MODIFICATIONS': False,
-        'SQLALCHEMY_BINDS': {
-            'users': 'sqlite:///database/users.db',
-            'blogs': 'sqlite:///database/blogs.db',
-            'items': 'sqlite:///database/items.db'
-        },
+        'SQLALCHEMY_BINDS': {table: f'{BASE_DB}{table}.db' for table in tables},
         'TEMPLATES_AUTO_RELOAD': True,
         'CACHE_TYPE': 'redis',
         'DEFAULT_AVATAR': r"C:\Users\dESKTOP I5\PycharmProjects\UrbeCafeBar\static\images\login-icon.jpg",
         'CORS_HEADERS': 'Content-Type'
     }
+    print(config_dict['SQLALCHEMY_BINDS'])
 
     app_dict.update(config_dict)
 
